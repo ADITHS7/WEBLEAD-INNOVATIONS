3572749cea4e06b4212bc029213ed2fef4ae3f3d
ADITHS7 <adithsn777@gmail.com>
2023-11-15 16:16:35 +0530

    Footer completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App-container">
       <BrowserRouter>
+      <Header/>
       <Footer/>
       </BrowserRouter>
     </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer'>
+      <div className="footer-top">
       <div className="footer-container">
         <img src={logo} alt="" className="logo" />
         <div className="logo-desc">
@@ -20,13 +21,39 @@ const Footer = () => {
 
       </div>
       <div className="footer-container">
-
+      <h2 className="foot-sub-head">EXPLORE</h2>
+      <div className="sub-child">
+        
+          <Link rel='' >About</Link>
+          <Link rel='' >Meet our team</Link>
+          <Link rel='' >Case stories</Link>
+          <Link rel='' >Latest news</Link>
+          <Link rel='' >Contact</Link>
+        </div>
+      </div>
+      <div className="footer-container">
+      <h2 className="foot-sub-head">CONTACT</h2>
+      <div className="sub-child">
+        
+          <p>66 Road broklyn street </p>
+          <a href=''>92########</a>
+          <a href=''>needhelp@company.com</a>
+        </div>
+      
       </div>
       <div className="footer-container">
         
       </div>
-      <div className="footer-container">
-        
+      </div>
+      <div className="footer-bottom">
+      <div className="footer-bar">
+        <div className="go-up">
+        <i class="fa-solid fa-caret-up"></i>
+        </div>
+        <div className="copy-right">
+        © Copyright 2023 by company.com
+        </div>
+      </div>
       </div>
     </div>
   )
